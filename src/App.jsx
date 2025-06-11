6bd79af0355f1e0267281befc76917c6af91083a
Aryan <bhardwaj7a@gmail.com>
2025-06-11 15:45:02 +0530

    Write basic toDo description and modifying functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
+// We are trying to make a to-do application that validates to do tasks for empty tasks and tasks with a word limit of 100. ToDos can be sorted 
 function App() {
-  const [count, setCount] = useState(0)
+const [todos , setTodos] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function addTodos(text) {
+  // convert text to a object and then add to the todos array
+  text = text.trim();
+  if(text.length() === 0 )
+  {
+    alert("A ToDo cannot be empty.");
+    return;
+  }
+  if(text.length > 100)
+  {
+    alert("A ToDo description cannot be greater than 100 characters. Try to make it a little bit coincise.");
+    return;
+  }
+  const task = {
+    id : crypto.randomUUID(),
+    completed : false,
+    text : text,
+    createdAt : Date.now()
+  };
+ // use the setTodos Function to add this todo into the existing todos
+ setTodos((prev) => ([...prev,task]))
 }
+
+
+function deleteTodo(id){
+  //filter the todos array based on this unique id created
+  todos.filter(todo => todo.id != id)
+}
+
+// function to sort todos based on their arrival order , i.e time of creation
+function sort() {
+  [...todos].sort((a,b) => b.createdAt - a.createdAt ) // [...todos creates a shallow copy ]
+}
+
+
+}
+
+
 
 export default App
